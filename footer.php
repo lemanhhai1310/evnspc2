4ea9b2517989c431179df1295051fce0d1ac861c
macbook <hai.lemanh.1310@gmail.com>
2019-04-05 01:48:22 +0700

    làm xong footer, đang làm tiếp slider trang chủ

--- footer.php
+++ footer.php
@@ -1,3 +1,39 @@
+<footer id="footer" class="uk-section-small">
+    <div class="uk-container uk-container-large">
+        <div uk-grid>
+            <div class="uk-width-auto@l">
+                <figure class="logo_f">
+                    <a href="#"><img src="imgs/logo.svg" alt=""></a>
+                </figure>
+            </div>
+            <div class="uk-width-expand@l">
+                <h3 class="title_f uk-text-uppercase">TỔNG CÔNG TY ĐIỆN LỰC MIỀN NAM</h3>
+                <div class="info_cty uk-margin-small-bottom">
+                    Cơ quan chủ quản: Tập đoàn Điện lực Việt Nam <br>
+                    Giấy phép số: 49/GP-TTĐT do Sở Thông tin và Truyền thông TP.HCM, cấp ngày 22/5/2015 <br>
+                    Trưởng ban biên tập: Ông Nguyễn Văn Hợp - Chủ tịch HĐTV Tổng công ty Điện lực miền Nam <br>
+                    Website: http://evnspc.vn
+                </div>
+                <div class="uk-margin-small author">© EVNSPC 2018. All right reserved</div>
+            </div>
+            <div class="uk-width-auto@l hotline">
+                <h3 class="title_f">Hỗ trợ trực tuyến 24/7: <span>19001006 - 19009000</span></h3>
+                <div class="info_cty uk-margin-small-bottom">
+                    Địa chỉ: 72 Hai Bà Trưng, P.Bến Nghé, Q. 1, Tp. Hồ Chí Minh <br>
+                    Điện thoại: (84 028) 3822 1605 - Fax: (84 028) 3822 1751 - 3939 0138 <br>
+                    Email:   info@evnspc.vn
+                </div>
+                <ul class="uk-grid-small uk-child-width-auto social_f" uk-grid>
+                    <li><a href="#"><span uk-icon="facebook"></span></a></li>
+                    <li><a href="#"><span uk-icon="twitter"></span></a></li>
+                    <li><a href="#"><span uk-icon="instagram"></span></a></li>
+                    <li><a href="#"><span uk-icon="youtube"></span></a></li>
+                </ul>
+                <div class="uk-margin-small author">Design by boconganh.net.vn</div>
+            </div>
+        </div>
+    </div>
+</footer>
 </section>
 </body>
 </html>
