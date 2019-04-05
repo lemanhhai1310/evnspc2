231408f99a49014bd7938bbb9fa92a6c794c5aed
macbook <hai.lemanh.1310@gmail.com>
2019-04-05 10:53:06 +0700

    update trangh chur

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Trang chủ - EVNSPC</title>
+    <link rel="shortcut icon" href="imgs/favicon-19.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <!--CSS-->
     <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
