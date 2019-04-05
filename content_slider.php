00b22c6f6ca3a65672b5e325ddd130eb56820818
macbook <hai.lemanh.1310@gmail.com>
2019-04-05 15:16:50 +0700

    fix slider

--- content_slider.php
+++ content_slider.php
@@ -1,7 +1,7 @@
 <div class="uk-container uk-container-large">
     <div class="uk-grid-stack" uk-grid>
         <div class="uk-width-1-2@m">
-            <h1 class="title_slide_h" uk-scrollspy="cls: uk-animation-scale-down; target: > span; delay: 500; repeat: true">
+            <h1 class="title_slide_h uk-margin-medium-bottom" uk-scrollspy="cls: uk-animation-scale-down; target: > span; delay: 500; repeat: true">
                 <span>Năng lượng xanh</span> <br>
                 <span>cho phát triển</span> <br>
                 <span>kinh tế bền vững</span>
