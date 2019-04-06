5b7cdf02f907652def9e294248c2a95f3784e77e
macbook <hai.lemanh.1310@gmail.com>
2019-04-07 06:01:53 +0700

    thêm click select language ngôn ngữ

--- fixed1.php
+++ fixed1.php
@@ -1,8 +1,22 @@
 <div class=" uk-position-fixed fixed1">
-    <div class="uk-margin">
+    <div class="uk-margin-small">
         <a uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="" class="uk-icon-button"></a>
     </div>
-    <div class="uk-margin">
-
+    <div class="uk-margin-small">
+        <div class="select_lang uk-text-center">
+            <div class="uk-flex uk-flex-column">
+                <a href="javascript: void(0);" onclick="myChange(this);" data-lang="vn">VN</a>
+                <a href="javascript: void(0);" onclick="myChange(this);" data-lang="en">EN</a>
+            </div>
+            <a href="#"><span uk-icon="plus"></span></a>
+        </div>
     </div>
 </div>
+<script>
+    function myChange(e) {
+        var tmp;
+        tmp = e.getAttribute("data-lang");
+        console.log(tmp);
+        document.getElementsByTagName("html")[0].setAttribute("lang", tmp)
+    }
+</script>
