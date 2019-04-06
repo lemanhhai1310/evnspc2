502e56ee10cae0bc6970026cd1330f22ab2d9a69
macbook <hai.lemanh.1310@gmail.com>
2019-04-07 05:06:08 +0700

    update trang home

--- fixed1.php
+++ fixed1.php
@@ -1,3 +1,8 @@
 <div class=" uk-position-fixed fixed1">
-    <a uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="" class="uk-icon-button"></a>
+    <div class="uk-margin">
+        <a uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="" class="uk-icon-button"></a>
+    </div>
+    <div class="uk-margin">
+
+    </div>
 </div>
