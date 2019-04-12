dbc5be89d1d386c50b0681a8314b82d22ff65f06
macbook <hai.lemanh.1310@gmail.com>
2019-04-13 01:57:27 +0700

    update trang tổng quan

--- vechungtoi-tongquan.php
+++ vechungtoi-tongquan.php
@@ -1,3 +1,15 @@
 <?php include('header.php'); ?>
-
+<div class="uk-container uk-container-large">
+    <div class="uk-card uk-card-primary uk-grid-collapse uk-flex-middle uk-margin" uk-grid>
+        <div class="uk-flex-last@s uk-width-2-3@s uk-card-media-right uk-cover-container">
+            <img src="imgs/banner_trangcon/vechungtoi-tongquan.png" alt="" uk-cover>
+            <canvas width="600" height="400"></canvas>
+        </div>
+        <div class="uk-width-expand@s">
+            <div class="uk-card-body">
+                <h3 class="uk-card-title uk-text-uppercase">tổng quan</h3>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include('footer.php'); ?>
