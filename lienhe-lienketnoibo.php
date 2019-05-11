62484f7215a9bedba3f38119045b4355d56b3f66
macbook <hai.lemanh.1310@gmail.com>
2019-05-11 23:22:46 +0700

    update

--- lienhe-lienketnoibo.php
+++ lienhe-lienketnoibo.php
@@ -19,7 +19,35 @@
                 <li><span>Liên kết nội bộ</span></li>
             </ul>
             <h2 class="uk-text-uppercase title_common uk-margin-medium-bottom">Liên kết nội bộ</h2>
-            <img src="imgs/MAP.svg" alt="">
+            <img class="uk-margin" src="imgs/MAP.svg" alt="">
+            <div class="uk-overflow-auto">
+                <table class="uk-table table3 uk-table-hover uk-table-middle uk-table-divider uk-table-striped">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <figure class="logo_f">
+                                <a href="#"><img src="imgs/Logos/Hậu%20Giang%20EVN.png" alt=""></a>
+                            </figure>
+                        </td>
+                        <td>
+                            <div>A: Phước Dân Town, Quận Ninh Phước, tỉnh Hậu Giang</div>
+                            <div>P: 0268.2210241 - F: 0268.2210241</div>
+                            <div>E: info@evn.com</div>
+                        </td>
+                    </tr>
+                    <?php for ($i=1;$i<=5;$i++) { ?>
+                    <tr>
+                        <td>Công ty điện lực Hậu Giang <?= $i; ?></td>
+                        <td>
+                            <div>A: Phước Dân Town, Quận Ninh Phước, tỉnh Hậu Giang</div>
+                            <div>P: 0268.2210241 - F: 0268.2210241</div>
+                            <div>E: info@evn.com</div>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
             <div class="uk-section-xsmall"></div>
         </div>
         <div class="uk-width-1-4@m uk-flex-first@m">
