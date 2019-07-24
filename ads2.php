20d9222d8c48e0dc9c2c8bedd341c20792d98453
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-07-24 14:48:18 +0700

    chia 3 file ads1 -> 3

---
+++ ads2.php
@@ -0,0 +1,214 @@
+<?php include('header.php'); ?>
+    <div class="block-ads uk-position-fixed uk-position-z-index">
+        <div class="uk-grid-collapse" hidden uk-grid>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%201.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="uk-grid-collapse uk-child-width-1-2@m" uk-grid>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%205.png" alt=""></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%206.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="uk-grid-collapse uk-child-width-1-3@m" hidden uk-grid>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%202.png" alt=""></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%203.png" alt=""></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                    <a class="uk-alert-close" uk-close></a>
+                    <a href="#"><img src="imgs/ads/EN/Layer%204.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+<div id="slider_home" class="uk-position-relative uk-visible-toggle" tabindex="-1" uk-slideshow="animation: push; min-height: 450; autoplay: true; pause-on-hover: true;">
+
+    <ul class="uk-slideshow-items" uk-height-viewport="offset-top: true">
+        <li>
+            <img src="imgs/slider/slider1.jpg" alt="" uk-cover>
+            <div class="uk-position-center uk-light" style="width: 100%;">
+                <?php include('content_slider.php'); ?>
+            </div>
+        </li>
+        <li>
+            <img src="imgs/slider/slider2.jpg" alt="" uk-cover>
+            <div class="uk-position-center uk-blue" style="width: 100%;">
+                <?php include('content_slider.php'); ?>
+            </div>
+        </li>
+        <li>
+            <img src="imgs/slider/slider3.jpg" alt="" uk-cover>
+            <div class="uk-position-center uk-light" style="width: 100%;">
+                <?php include('content_slider.php'); ?>
+            </div>
+        </li>
+    </ul>
+
+    <div class="uk-light">
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+    </div>
+
+    <ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin uk-position-bottom-center uk-position-z-index"></ul>
+
+    <div id="menu_link_slide" class="uk-position-bottom uk-visible@m">
+        <div class="uk-container uk-container-large">
+            <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+                <div class="uk-navbar-left">
+
+                    <ul class="uk-navbar-nav">
+                        <li><a href="#">thông tin dự án</a></li>
+                        <li><a href="#">quan hệ quốc tế</a></li>
+                        <li><a href="#">dịch vụ trực tuyến</a></li>
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+    </div>
+    <a href="#footer" class="icon-scroll uk-position-bottom-center uk-hidden" uk-scroll></a>
+</div>
+<div class="uk-section-small">
+    <div class="uk-container uk-container-large uk-margin-medium">
+        <h3 class="title_partner uk-text-uppercase uk-text-center">đối tác</h3>
+        <div id="slider_brand" uk-slider="autoplay: true; autoplay-interval: 3000;">
+
+            <div class="uk-position-relative">
+
+                <div class="uk-slider-container uk-light">
+                    <ul class="uk-slider-items uk-text-center uk-flex-middle uk-child-width-1-2 uk-child-width-1-3@m uk-child-width-1-5@l" uk-grid>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/CFE.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/Kepco.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/aes%20dot%20com.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/nepal.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/tenaga.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/CFE.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/Kepco.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/aes%20dot%20com.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/nepal.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div class="uk-padding">
+                                    <img src="imgs/brand/tenaga.png" alt="">
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="uk-hidden@s uk-light">
+                    <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                </div>
+
+                <div class="uk-visible@s">
+                    <a class="uk-position-center-left uk-position-small uk-margin-remove-left uk-margin-remove-right uk-padding-remove-left uk-padding-remove-right" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a class="uk-position-center-right uk-position-small uk-margin-remove-left uk-margin-remove-right uk-padding-remove-left uk-padding-remove-right" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div class="uk-container uk-container-large">
+        <div class="uk-flex-middle" uk-grid>
+            <div class="uk-width-auto@l uk-visible@m">
+                <figure class="logo_f uk-invisible">
+                    <a href="#"><img src="imgs/logo.svg" alt=""></a>
+                </figure>
+            </div>
+            <div class="uk-width-expand@l">
+                <form class="uk-grid-small uk-grid-collapse box_register uk-grid" uk-grid="">
+                    <div class="uk-width-expand uk-first-column">
+                        <input class="uk-input" type="email" placeholder="Đăng ký email ở đây để nhận các thông tin mới nhất từ EVNSPC">
+                    </div>
+                    <div class="uk-width-auto">
+                        <button type="submit" class="uk-button uk-button-primary">Gửi</button>
+                    </div>
+                </form>
+            </div>
+            <div class="uk-width-1-6@l uk-visible@m">
+                <figure class="logo_f uk-invisible uk-visible@l">
+                    <a href="#"><img src="imgs/logo.svg" alt=""></a>
+                </figure>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>
--- block-ads.php
+++ block-ads.php
@@ -1,42 +1,44 @@
-<div class="uk-grid-collapse" uk-grid>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%201.png" alt=""></a>
+<div class="block-ads uk-position-fixed uk-position-z-index">
+    <div class="uk-grid-collapse" uk-grid>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%201.png" alt=""></a>
+            </div>
         </div>
     </div>
-</div>
-<div class="uk-grid-collapse uk-child-width-1-2@m" uk-grid>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%205.png" alt=""></a>
+    <div class="uk-grid-collapse uk-child-width-1-2@m" hidden uk-grid>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%205.png" alt=""></a>
+            </div>
+        </div>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%206.png" alt=""></a>
+            </div>
         </div>
     </div>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%206.png" alt=""></a>
+    <div class="uk-grid-collapse uk-child-width-1-3@m" hidden uk-grid>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%202.png" alt=""></a>
+            </div>
         </div>
-    </div>
-</div>
-<div class="uk-grid-collapse uk-child-width-1-3@m" uk-grid>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%202.png" alt=""></a>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%203.png" alt=""></a>
+            </div>
         </div>
-    </div>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%203.png" alt=""></a>
-        </div>
-    </div>
-    <div>
-        <div class="uk-margin-remove uk-padding-remove" uk-alert>
-            <a class="uk-alert-close" uk-close></a>
-            <a href="#"><img src="imgs/ads/EN/Layer%204.png" alt=""></a>
+        <div>
+            <div class="uk-margin-remove uk-padding-remove" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <a href="#"><img src="imgs/ads/EN/Layer%204.png" alt=""></a>
+            </div>
         </div>
     </div>
 </div>
